830eec3837e7cff4e2609c28dad85c05f2990b14
Per Stark <perstark.se@gmail.com>
2024-06-15 17:30:13 +0200

    fixed merge yo

--- pkgs/by-name/ls/lsp-ai/package.nix
+++ pkgs/by-name/ls/lsp-ai/package.nix
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-{ lib, fetchCrate, rustPlatform }:
-
-rustPlatform.buildRustPackage rec {
-pname = "lsp-ai";
-version = "0.3.0";
-
-src = fetchCrate {
-inherit pname version;
-sha256 = lib.fakeSha256;
-};
-
-cargoHash = lib.fakeSha256;
-
-meta = with lib; {
-description = "LSP-AI is an open-source language server that serves as a backend for AI-powered functionality, designed to assist and empower software engineers, not replace them.";
-homepage = "https://github.com/SilasMarvin/lsp-ai";
-license = licenses.mit;
-maintainers = [maintainers.perstarkse];
-};
-=======
 { lib, fetchCrate, rustPlatform, pkgs }:
 
 rustPlatform.buildRustPackage rec {
@@ -62,5 +41,4 @@ rustPlatform.buildRustPackage rec {
     license = licenses.mit;
     maintainers = [maintainers.perstarkse];
   };
->>>>>>> lsp-ai-init
 }
